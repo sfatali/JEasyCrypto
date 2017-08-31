f71bec9fa07bbd1dbf02a2092b11437361ce9a04
Antti Juustila <antti.juustila@gmail.com>
2017-08-31 20:07:47 +0300

    Cleaning code

--- JEasyCryptoConsole/src/EasyCryptoConsole/EasyCryptoConsole.java
+++ JEasyCryptoConsole/src/EasyCryptoConsole/EasyCryptoConsole.java
@@ -3,10 +3,6 @@ package EasyCryptoConsole;
 import java.io.Console;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
-import java.nio.charset.Charset;
-import java.text.BreakIterator;
-import java.util.Locale;
-
 import EasyCryptoLib.EasyCryptoLib;
 
 
@@ -15,17 +11,6 @@ public class EasyCryptoConsole {
 	public static void main(String[] args) {
 
 		try {
-		    String antti = "\u2234";
-		    System.out.println(antti);
-		    
-		    for (int codePoint = 0x1F600; codePoint <= 0x1F64F;) {
-		        System.out.print(Character.toChars(codePoint));
-		        codePoint++;
-		        if (codePoint % 16 == 0) {
-		            System.out.println();
-		        }
-		    }
-		    
 			System.setOut(new PrintStream(System.out, true, "UTF-8"));
 			System.setProperty("file.encoding", "UTF-8");
 			Console console = System.console();
