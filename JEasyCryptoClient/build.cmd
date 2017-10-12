0914922428900e8bf949de4f5d9c9053995ef33d
Risto Juntunen <risju62@windowslive.com>
2017-10-12 17:33:41 +0300

    Fix for windows build issue #22
    Build and start script for windows added.
    Console start.cmd works now.

---
+++ JEasyCryptoClient/build.cmd
@@ -0,0 +1,3 @@
+javac src\*.java -classpath "..\json-simple-1.1.1.jar;." -d bin
+exit
+
---
+++ build.cmd
@@ -0,0 +1,12 @@
+cd JEasyCrypto
+start /B /WAIT build.cmd
+cd ..
+cd .\JEasyCryptoClient
+start /B /WAIT build.cmd
+cd ..
+cd .\JEasyCryptoConsole
+start /B /WAIT build.cmd
+cd ..
+cd .\JEasyCryptoServer
+start /B /WAIT build.cmd
+cd ..
