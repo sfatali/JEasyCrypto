59d250bb80d287844e46c1f5276a05116980e1e3
Alejandro Ramirez <armzprz@gmail.com>
2017-10-18 13:02:09 +0300

    Add details on build steps and clear script

--- build.cmd
+++ build.cmd
@@ -1,12 +1,33 @@
+start /B /WAIT clear.cmd
+echo ""
+echo ""
+
+echo "Building JEasyCrypto lib"
 cd JEasyCrypto
 start /B /WAIT build.cmd
 cd ..
+echo ""
+echo ""
+
+echo "Building JEasyCryptoClient"
 cd .\JEasyCryptoClient
 start /B /WAIT build.cmd
 cd ..
+echo ""
+echo ""
+
+echo "Building JEasyCryptoConsole"
 cd .\JEasyCryptoConsole
 start /B /WAIT build.cmd
 cd ..
+echo ""
+echo ""
+
+echo "Building JEasyCryptoServer"
 cd .\JEasyCryptoServer
 start /B /WAIT build.cmd
 cd ..
+echo ""
+echo ""
+
+echo "Build done"
