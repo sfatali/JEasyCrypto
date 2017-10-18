709a28197d23c3555c1196a973ee47fc332e639f
Alejandro Ramirez <armzprz@gmail.com>
2017-10-18 13:02:09 +0300

    Add details on build steps and clear script

--- build.sh
+++ build.sh
@@ -1,11 +1,32 @@
+sh clear.sh
+echo ""
+echo ""
+
+echo "Building JEasyCrypto lib"
 cd ./JEasyCrypto
 sh build.sh
 cd ..
+echo ""
+echo ""
+
+echo "Building JEasyCryptoClient"
 cd ./JEasyCryptoClient
 sh build.sh
 cd ..
+echo ""
+echo ""
+
+echo "Building JEasyCryptoConsole"
 cd ./JEasyCryptoConsole
 sh build.sh
 cd ..
+echo ""
+echo ""
+
+echo "Building JEasyCryptoServer"
 cd ./JEasyCryptoServer
 sh build.sh
+echo ""
+echo ""
+
+echo "Build done"
