155213cea16b58b72713997f71d5c58fb866e614
Alejandro Ramirez <armzprz@gmail.com>
2017-10-18 13:02:33 +0300

    Add script to clean previous builds

---
+++ clear.cmd
@@ -0,0 +1,13 @@
+echo "Removing previous build files"
+
+rm JEasyCrypto/bin/easycrypto/*
+echo "JEasyCrypto lib clean"
+
+rm JEasyCryptoClient/bin/*
+echo "JEasyCryptoClient clean"
+
+rm JEasyCryptoConsole/bin/EasyCryptoConsole/*
+echo "JEasyCryptoConsole clean"
+
+rm JEasyCryptoServer/bin/*
+echo "JEasyCryptoServer clean"
