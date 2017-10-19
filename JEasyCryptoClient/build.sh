62e8d12bdd4139f23faba6673be21617d4d6fad9
Daniel Toniuc <dtoniuc@gmail.com>
2017-10-19 22:03:52 +0300

    Add mkdir to linux scripts
    Mkdir was added in all build scripts to create the bin folders

--- JEasyCryptoClient/build.sh
+++ JEasyCryptoClient/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+mkdir bin
 find -name "*.java" > sources.txt
 javac @sources.txt -classpath "../json-simple-1.1.1.jar:." -d bin
 rm sources.txt
--- JEasyCryptoConsole/build.sh
+++ JEasyCryptoConsole/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
 
+mkdir bin
 javac src/EasyCryptoConsole/*.java -classpath "../EasyCryptoLib.jar:." -d bin
